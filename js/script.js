8666cba50be8fb1725bf1a3b2321a352d3d5a7ae
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-30 16:14:38 +0000

    Fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha Vorontsov
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+
+function calculaterandomnumber () {
+
+  const ageNumber = 6 || 7 || 8 || 9 || 10 || 11 || 12 || 13 || 14 || 15 || 16 || 17 || 18
+  const day= document.getElementById('option-friday').checked
+  const day1= document.getElementById('option-saturday').checked
+  
+  if ( (day || day1)||(ageNumber)  == true )  {
+    document.getElementById('answer1').innerHTML =
+      "<p>You can get a discount for the playplace</p>" 
+  }
+  else {
+    document.getElementById('answer1').innerHTML =
+      "<p>The Normal Price It Is</p>" 
+  }
+
+}
